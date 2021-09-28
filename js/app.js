5dc9b12f94f6c1f7aae45f8421bb72a8535eeeea
jepkess <biivincent1997@gmail.com>
2021-09-29 01:22:13 +0300

    modify all the pages

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
-// var myname = prompt("enter your name to continue.")
-// document.getElementById("username").innerHTML = myname;
-
 function besure() {
-//    var check = confirm("Do you really want to continue")
+    //    var check = confirm("Do you really want to continue")
 
-   if (confirm("Do you really want to continue")){
-       window.location.href = 'work.html';
-       return true;
-       
-   } else  {
-       window.location.href = '/';
-       return false;
-   }
+    if (confirm("Do you really want to continue")) {
+        window.location.href = 'work.html';
+
+
+        return true;
+
+    } else {
+        window.location.href = '/';
+        return false;
+    }
 
 }
+
+function getname() {
+    var myname = prompt("enter your name to continue.")
+    document.getElementById("username").innerHTML = myname;
+    return true;
+
+
+}
+
+//hide and show effects (Jquery)
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("p").hide();
+    });
+    $("#show").click(function () {
+        $("p").show();
+    })
+});
